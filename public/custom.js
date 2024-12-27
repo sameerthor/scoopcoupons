8ed30370e76b00bf87bc4b1c774af5a51c28a9dc
shivam4srtech <shivam.srtech@gmail.com>
2024-12-27 09:53:34 +0530

    modified header script

--- public/custom.js
+++ public/custom.js
@@ -1,16 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     const showInputBtn = document.querySelector('.showInputBtn');
     const searchform = document.querySelector('#searchform');
+    
+    const handleToggle = function () {
+        showInputBtn.addEventListener('click', function (event) {
+            event.stopPropagation();
+            searchform.style.display = searchform.style.display === 'flex' ? 'none' : 'flex';
+        });
 
-    showInputBtn.addEventListener('click', function (event) {
-        event.stopPropagation();
-        searchform.style.display = searchform.style.display === 'flex' ? 'none' : 'flex';
-    });
+        document.addEventListener('click', function () {
+            searchform.style.display = 'none';
+        });
 
-    document.addEventListener('click', function () {
-        searchform.style.display = 'none';
-    });
-    searchform.addEventListener('click', function (event) {
-        event.stopPropagation();
+        searchform.addEventListener('click', function (event) {
+            event.stopPropagation();
+        });
+    };
+
+    const mediaQuery = window.matchMedia('(max-width: 769px)');
+    if (mediaQuery.matches) {
+        handleToggle();
+    }
+
+    mediaQuery.addEventListener('change', function (e) {
+        if (e.matches) {
+            handleToggle();
+        } else {
+            showInputBtn.removeEventListener('click', handleToggle);
+            document.removeEventListener('click', handleToggle);
+            searchform.removeEventListener('click', handleToggle);
+            searchform.style.display = 'none';
+        }
     });
 });
